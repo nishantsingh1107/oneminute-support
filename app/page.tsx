2af64b0093dda4553c509e452cc140ba3ee6dfae
nishantsingh1107 <nishantsingh0311@gmail.com>
2026-07-20 10:59:46 +0530

    Add landing page components: Features, Footer, Integration, Pricing, and SocialProof

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Features from '@/components/landing/features'
+import Footer from '@/components/landing/footer'
 import Hero from '@/components/landing/hero'
+import Integration from '@/components/landing/integration'
 import Navbar from '@/components/landing/nav'
+import Pricing from '@/components/landing/pricing'
+import SocialProof from '@/components/landing/social'
 import React from 'react'
 
 const page = () => {
@@ -7,6 +12,11 @@ const page = () => {
     <main className='w-full flex flex-col relative z-10'>
       <Navbar />
       <Hero />
+      <SocialProof />
+      <Features />
+      <Integration />
+      <Pricing />
+      <Footer />
     </main>
   )
 }
